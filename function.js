0011fb42a6bb88a3cddc5b45c2d21f936c314325
hexadi <38806729+hexadi@users.noreply.github.com>
2019-04-12 12:03:32 +0700

    add cfonts and welcome

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ var txt = "";
 var path = `./cat-${new Date().getDate()}-${new Date().getMonth()+1}-${new Date().getYear()+1900}.log`;
 const axios = require('axios');
 const fs = require('fs');
-
+const CFonts = require('cfonts');
 
 function get(response) {
     response.data.now == null ?
@@ -19,7 +19,22 @@ function fetchData() {
             console.log(err);
         });
 }
+
+function welcome() {
+    CFonts.say('Cat Listening History', {
+        font: 'chrome', // define the font face
+        align: 'center', // define text alignment
+        colors: ['white'], // define all colors
+        background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1, // define letter spacing
+        lineHeight: 1, // define the line height
+        space: true, // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0', // define how many character can be on one line
+    });
+}
+
 function main() {
+    welcome();
     fs.readFile(path,
         function (err, data) {
           if (err) {console.log(err);} else {txt = data;}
